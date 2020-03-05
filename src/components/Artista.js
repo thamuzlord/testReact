ad80497682d4afa2fb65ba5e8de3996f396c7227
vascojs <vasco.1019@gmail.com>
2020-03-05 01:18:12 -0500

    fix bug in Artista

--- src/components/Artista.js
+++ src/components/Artista.js
@@ -3,12 +3,10 @@ import PropTypes from 'prop-types';
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
 const Artista = ({image, name, id}) => (
-    <Router>
         <div  id="container">
             <img className="img-circle" src={image} alt={name} />
             <h3 className="centeredText" id={id}><Link to={`/AlbumContainer/${id}`}>{name}</Link></h3>
         </div>
-    </Router>
 )
 
 Artista.propTypes = {

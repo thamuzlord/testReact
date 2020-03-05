829b808cc787eed65781a01565a1e21b4d47d8cd
vascojs <vasco.1019@gmail.com>
2020-03-05 13:14:22 -0500

    Updates

---
+++ precache-manifest.040cb16ec750558a02c29927caf5ed85.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b96050a903ba9112a5b14a7ad9e1b8c3",
+    "url": "/good-music/index.html"
+  },
+  {
+    "revision": "c0cc629ce316aba42dc8",
+    "url": "/good-music/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "970772c12ec85f62a555",
+    "url": "/good-music/static/css/main.03c4fdd6.chunk.css"
+  },
+  {
+    "revision": "c0cc629ce316aba42dc8",
+    "url": "/good-music/static/js/2.6239b7ff.chunk.js"
+  },
+  {
+    "revision": "a02c2e6156cb6e9e5c30be88d1ba3366",
+    "url": "/good-music/static/js/2.6239b7ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "970772c12ec85f62a555",
+    "url": "/good-music/static/js/main.eaf10f54.chunk.js"
+  },
+  {
+    "revision": "56573cbf51cb8a0dc983",
+    "url": "/good-music/static/js/runtime-main.2080ef5c.js"
+  }
+]);
